23347dea2d4e52bee1faca030c22805ae4dd6072
Roy Portas <royportas@gmail.com>
2025-09-14 13:56:25 +1000

    Init project

---
+++ README.md
@@ -0,0 +1,17 @@
+# Registry 
+
+## Usage
+
+> TODO: Steps for setting up and using the registry
+
+## Development
+
+Install [Bun](https://bun.sh/) and run the following commands:
+
+```bash
+# Install dependencies
+bun install
+
+# Start the development server
+bun dev
+```
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function Home() {
+  return (
+    <main>
+      <Card className="m-4">
+        <CardHeader>
+          <CardTitle>Registry</CardTitle>
+        </CardHeader>
+      </Card>
+    </main>
+  );
+}
